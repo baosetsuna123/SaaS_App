db2c4dde571e953abca66b6b718d8eec6aa1e85f
baosetsuna <baohse321@gmail.com>
2025-12-20 12:06:00 +0700

    Integrate clerk auth & billing

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,10 @@
-const Subscription = () => {
-  return (
-    <div>Subscription</div>
-  )
-}
+import {PricingTable} from "@clerk/nextjs";
 
+const Subscription = () => {
+    return (
+        <div>
+            <PricingTable />
+        </div>
+    )
+}
 export default Subscription
